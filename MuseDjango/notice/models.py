7d373f77f98845ab2e4ca485fede84647333e893
KyoungnamMin <alsrudska97@naver.com>
2021-12-31 12:27:45 +0900

    [MUSE-021] 배너 추가

--- MuseDjango/notice/models.py
+++ MuseDjango/notice/models.py
@@ -7,6 +7,12 @@ NOTICE_CATEGORY = (
     ("guide", "이용 안내"),
     ("general", "일반")("footer", "사이트 설명"),
 )
+BANNER_CATEGORY = (
+    ("main_banner", "메인 배너"),
+    ("muse_banner", "뮤즈 배너"),
+    ("contest_banner", "콘테스트 배너"),
+    ("reference_banner", "레퍼런스 배너"),
+)
 
 
 class Notice(models.Model):
@@ -28,4 +34,26 @@ class Notice(models.Model):
 
     class Meta:
         db_table = "Notice"
-        verbose_name_plural = "MUSE 공지사항"
+        verbose_name_plural = "공지사항"
+
+
+class Banner(models.Model):
+    idx = models.AutoField(primary_key=True, blank=False, null=False)
+    category = models.CharField(
+        max_length=30,
+        blank=True,
+        null=True,
+        choices=BANNER_CATEGORY,
+        verbose_name="배너 카테고리",
+    )
+    title = models.CharField(max_length=1000, blank=True, null=True, verbose_name="제목")
+    content = models.CharField(
+        max_length=1000, blank=True, null=True, verbose_name="내용"
+    )
+    usage = models.BooleanField(default=True, verbose_name="노출 여부")
+    created_at = models.DateTimeField(verbose_name="최초 업로드 날짜", auto_now_add=True)
+    modified_at = models.DateTimeField(verbose_name="최근 수정 날짜", auto_now=True)
+
+    class Meta:
+        db_table = "Banner"
+        verbose_name_plural = "배너"
